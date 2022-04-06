b94da8da04b7a9a842a3c302dbf4cd5edcad186a
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-04-06 17:43:58 -0400

    first commit, this contains our code to write in

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Link to="/"> Home Page</Link>
+
+      {'   |    '}
+
+      <Link to="/coins">Coin information</Link>
+
+      {'   |    '}
+
+      <Link to="/form">Form</Link>
+
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/coins" element={<Coins />}></Route>
+        <Route path="/form" element={<Form />}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+function HomePage() {
+  return (
+    <div>
+      <h1> HOME PAGE JONATHAN SECTION, using sass, blockchain info </h1>
+    </div>
+  );
+}
+
+function Call() {
+  return (
+    <div>
+      <h1> ZACK AXIOS API CALL to coingecko api </h1>
+    </div>
+  );
+}
+
+function Coins() {
+  return (
+    <div>
+      <h1> JOYDEEP DISPLAYING COINS price data </h1>
+    </div>
+  );
+}
+
+function Form() {
+  return (
+    <div>
+      <h1> Eliaf form component part for audince input </h1>
     </div>
   );
 }
